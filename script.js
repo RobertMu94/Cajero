a380ee51ef29d970fe5a99d53b74f34b1e123cad
unknown <robertmunoz@misena.edu.co>
2023-08-07 23:23:16 -0500

    avance1

---
+++ script.js
@@ -0,0 +1,10 @@
+var cuentas = [
+    { nombre: "Robert", saldo: 200 ,contraseña: "123456"},
+    { nombre: "Martin", saldo: 290 ,contraseña: "654321"},
+    { nombre: "Juan", saldo: 67 ,contraseña: "987654"},
+  ];
+
+  function Ingresoformulario(){
+
+    let user=document.getElementById("")
+  }
