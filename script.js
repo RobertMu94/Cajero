d84162bd24eadce8bf3e5d9fe3e1b795db2fa4fd
unknown <robertmunoz@misena.edu.co>
2023-08-08 00:29:43 -0500

    avancelogin

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-var cuentas = [
-    { nombre: "Robert", saldo: 200 ,contraseña: "123456"},
-    { nombre: "Martin", saldo: 290 ,contraseña: "654321"},
-    { nombre: "Juan", saldo: 67 ,contraseña: "987654"},
-  ];
+    var cuentas = [
+        { nombre: "Robert", saldo: 200, contraseña: "123456" },
+        { nombre: "Martin", saldo: 290, contraseña: "654321" },
+        { nombre: "Juan", saldo: 67, contraseña: "987654" },
+    ];
 
-  function Ingresoformulario(){
+function Ingresoformulario() {
 
-    let user=document.getElementById("")
-  }
+    var user = document.getElementById("usuario").value;
+    var pass = document.getElementById("clave").value;
+
+    for (var i = 0; i < cuentas.length; i++) {
+        if (cuentas[i].nombre === user && cuentas[i].contraseña === pass) {
+            window.location.href ="index2.html";
+            return;
+        }
+    }
+  alert("usuario o contraseña incorrecta. Intentalo de nuevo");
+}
+ 
